ea09cc7a2d5dda10f56003bbccc5fdb814388feb
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2024-02-11 16:18:32 -0500

    login

--- src/login.java
+++ src/login.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class login {
+    private JTextField modoTextField;
+    private JTextField codigoUnicoTextField;
+    private JTextField textField4;
+    private JTextField contraseñaTextField;
+    private JButton ingresarButton;
+    private JButton salirButton;
+    private JPasswordField passwordField1;
+    private JComboBox comboBox1;
 }
