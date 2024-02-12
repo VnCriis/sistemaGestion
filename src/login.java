9d6a2cdb2b04ebfc515f70506da84970e5fa1549
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2024-02-11 23:29:59 -0500

    interfaz

--- src/login.java
+++ src/login.java
@@ -1,10 +1,7 @@
 import javax.swing.*;
 
 public class login {
-    private JTextField modoTextField;
-    private JTextField codigoUnicoTextField;
     private JTextField textField4;
-    private JTextField contraseñaTextField;
     private JButton ingresarButton;
     private JButton salirButton;
     private JPasswordField passwordField1;
