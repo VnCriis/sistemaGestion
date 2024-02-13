31926e748874c9c610785794d1741c85296493a2
Cristian Usiña <cristian.usina@epn.edu.ec>
2024-02-13 09:32:25 -0500

    Relación de primeras pantallas relacion rol con su pantalla respectiva

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame login = new JFrame("");
+        login.setContentPane(new login().loginJPanel);
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.pack();
+        login.setSize(850,420);
+        login.setVisible(true);
     }
 }
--- src/login.java
+++ src/login.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class login {
     private JTextField textField4;
@@ -6,4 +8,35 @@ public class login {
     private JButton salirButton;
     private JPasswordField passwordField1;
     private JComboBox comboBox1;
+    JPanel loginJPanel;
+
+    public login() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String opcionSeleccionada = (String) comboBox1.getSelectedItem(); // Corrección: Utiliza getSelectedItem()
+                if (opcionSeleccionada != null) {
+                    switch (opcionSeleccionada) {
+                        case "Estudiante":
+                            JFrame estudiante = new JFrame("");
+                            estudiante.setContentPane(new estudiante().estudianteJPanel);
+                            estudiante.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            estudiante.pack();
+                            estudiante.setSize(850,420);
+                            estudiante.setVisible(true);
+                            break;
+                        case "Tutor":
+                            JFrame tutor = new JFrame("");
+                            tutor.setContentPane(new tutor().tutorJPanel);
+                            tutor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            tutor.pack();
+                            tutor.setSize(850,420);
+                            tutor.setVisible(true);
+                            break;
+                    }
+                }
+            }
+
+        });
+    }
 }
