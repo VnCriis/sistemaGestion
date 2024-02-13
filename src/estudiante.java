655bcb4dd20043851f3f35732a1642fc5a7d169a
Cristian Usiña <cristian.usina@epn.edu.ec>
2024-02-13 16:46:46 -0500

    creacion de pantallas de opciones de estudiantes

--- src/estudiante.java
+++ src/estudiante.java
@@ -3,4 +3,59 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class estudiante {
     JPanel estudianteJPanel;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton4;
+
+    public estudiante() {
+        radioButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame estudianteOL = new JFrame("");
+                estudianteOL.setContentPane(new estudianteOL().olJPanel);
+                estudianteOL.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                estudianteOL.pack();
+                estudianteOL.setSize(850,420);
+                estudianteOL.setVisible(true);
+                ((JFrame) SwingUtilities.getWindowAncestor(radioButton1)).dispose();
+            }
+        });
+        radioButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame estudianteOV = new JFrame("");
+                estudianteOV.setContentPane(new estudianteOV().ovJPanel);
+                estudianteOV.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                estudianteOV.pack();
+                estudianteOV.setSize(850,420);
+                estudianteOV.setVisible(true);
+                ((JFrame) SwingUtilities.getWindowAncestor(radioButton2)).dispose();
+            }
+        });
+        radioButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame estudianteFormularios = new JFrame("");
+                estudianteFormularios.setContentPane(new estudianteFormularios().formulariosJPanel);
+                estudianteFormularios.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                estudianteFormularios.pack();
+                estudianteFormularios.setSize(850,420);
+                estudianteFormularios.setVisible(true);
+                ((JFrame) SwingUtilities.getWindowAncestor(radioButton3)).dispose();
+            }
+        });
+        radioButton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame estudianteInicio = new JFrame("");
+                estudianteInicio.setContentPane(new estudianteInicio().inicioJPanel);
+                estudianteInicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                estudianteInicio.pack();
+                estudianteInicio.setSize(850,420);
+                estudianteInicio.setVisible(true);
+                ((JFrame) SwingUtilities.getWindowAncestor(radioButton4)).dispose();
+            }
+        });
+    }
 }
--- src/estudiante_OfertasPracticas.java
+++
@@ -1,2 +0,0 @@
-public class estudiante_OfertasPracticas {
-}
